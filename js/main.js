0c0aeb5dc31310ff4c3366d690c9432dc3c0b7b9
David Stobbe <d.stobbe1@share.epsb.ca>
2022-10-04 12:54:41 -0600

    pretty much done

--- js/main.js
+++ js/main.js
@@ -7,13 +7,58 @@ let signUpBtn = document.getElementById('sign-up-btn');
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
 
+let users = JSON.parse(localStorage.getItem("users")) ?? [];
 function signUpHandler() {
   console.log('Sign Up Btn Clicked');
+  let username = document.getElementById('usernameInput').value
+  let password  = document.getElementById('passwordInput').value
+  let pswdConfirmation = document.getElementById('confirmPswdInput').value
+  let valid = true
+  for(let i = 0; i < users.length; i++) {
+    if(users[i].username !== username) {
+      valid = false
+      alert("username already taken")
+      break;
+    }
+  }
+
+  if(valid) {
+    if(password === pswdConfirmation) {
+      users.push(newUser(username, password))
+      save()
+      alert("Signed up!")
+    } else {
+      alert("both passwords must be identical")
+    }
+  }
 }
 
 // SIGN IN BTN CLICKED
 signInBtn.addEventListener('click', signInHandler);
 
+function newUser(username, userPassword) {
+  return {
+    name: username,
+    password: userPassword,
+  }
+}
+
+function save() {
+  localStorage.setItem("users", JSON.stringify(users))
+}
+
 function signInHandler() {
-  console.log('Sign In Btn Clicked');
+  let username = document.getElementById('usernameInput').value
+  let password  = document.getElementById('passwordInput').value
+  for(let i = 0; i < users.length; i++) {
+    if(username === users[i].name) {
+      if(password !== users[i].password) {
+        alert("Not a valid password")
+        break;
+      } else {
+        alert("Success!")
+      }
+    }
+  }
+  
 }
