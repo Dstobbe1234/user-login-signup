40d766a323cfbea2f75dd340e93ea07d666a01ad
David Stobbe <d.stobbe1@share.epsb.ca>
2022-10-10 14:37:38 -0600

    done

--- js/main.js
+++ js/main.js
@@ -1,71 +1,71 @@
 // USER LOGIN / SIGNUP
 
 // HTML VARIABLES
-let signInBtn = document.getElementById('sign-in-btn');
-let signUpBtn = document.getElementById('sign-up-btn');
+let signInBtn = document.getElementById("sign-in-btn");
+let signUpBtn = document.getElementById("sign-up-btn");
 
 // SIGN UP BTN CLICKED
-signUpBtn.addEventListener('click', signUpHandler);
+signUpBtn.addEventListener("click", signUpHandler);
 
 let users = JSON.parse(localStorage.getItem("users")) ?? [];
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
-  let username = document.getElementById('usernameInput').value
-  let password  = document.getElementById('passwordInput').value
-  let pswdConfirmation = document.getElementById('confirmPswdInput').value
-  let valid = true
-  if(username !== "" && password !== "" && pswdConfirmation !== "") {
-    for(let i = 0; i < users.length; i++) {
-      console.log(i)
-      if(users[i].name === username) {
-        valid = false
-        alert("username already taken")
+  console.log("Sign Up Btn Clicked");
+  let username = document.getElementById("usernameInput").value;
+  let password = document.getElementById("passwordInput").value;
+  let pswdConfirmation = document.getElementById("confirmPswdInput").value;
+  let valid = true;
+  if (username !== "" && password !== "" && pswdConfirmation !== "") {
+    for (let i = 0; i < users.length; i++) {
+      console.log(i);
+      if (users[i].name === username) {
+        valid = false;
+        alert("username already taken");
         break;
       }
     }
-    if(valid) {
-      if(password === pswdConfirmation) {
-        users.push(newUser(username, password))
-        save()
-        alert("Signed up!")
+    if (valid) {
+      if (password === pswdConfirmation) {
+        users.push(newUser(username, password));
+        save();
+        alert("Signed up!");
       } else {
-        alert("both passwords must be identical")
+        alert("both passwords must be identical");
       }
     }
   } else {
-    alert("all required fields must be filled")
+    alert("all required fields must be filled");
   }
 }
 
 // SIGN IN BTN CLICKED
-signInBtn.addEventListener('click', signInHandler);
+signInBtn.addEventListener("click", signInHandler);
 
 function newUser(username, userPassword) {
   return {
     name: username,
     password: userPassword,
-  }
+  };
 }
 
 function save() {
-  localStorage.setItem("users", JSON.stringify(users))
+  localStorage.setItem("users", JSON.stringify(users));
 }
 
 function signInHandler() {
-  let username = document.getElementById('usernameInput').value
-  let password  = document.getElementById('passwordInput').value
-  let valid = false
-  console.log(password)
-  for(let i = 0; i < users.length; i++) {
-    console.log(users[i].password)
-    if(username === users[i].name && password === users[i].password) {
-      alert("Success!")
-      valid = true
+  let username = document.getElementById("loginUsername").value;
+  let password = document.getElementById("loginPassword").value;
+  let valid = false;
+  console.log("sign in button clicked");
+  console.log(password);
+  for (let i = 0; i < users.length; i++) {
+    if (username === users[i].name && password === users[i].password) {
+      alert("Success!");
+      valid = true;
       break;
     }
   }
 
-  if(!valid) {
-    alert("Wrong!")
+  if (!valid) {
+    alert("Wrong!");
   }
 }
